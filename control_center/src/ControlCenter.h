81cb36803470ca1121705fdb74d6b39e3f2c5e85
Nick <nclmda@gmail.com>
2024-01-23 23:15:24 +0100

    functions definitions

--- control_center/src/ControlCenter.h
+++ control_center/src/ControlCenter.h
@@ -1,21 +1,33 @@
 #ifndef CONTROL_CENTER_H
 #define CONTROL_CENTER_H
 #include <vector>
-#include "Drone.h" // Includi il file dell'header della classe Drone
+#include <time.h>
+#define WIDTH 600 //since we want points 10m apart
+#define HEIGHT 600
+
 
 class ControlCenter {
 private:
-    std::vector<Drone> drones;
-    time * grid;// griglia 2d ultima ora di verifica
+    std::vector<Drone> drones;// vector of drones ids, make another drone class
+    time_t * grid[WIDTH][WIDTH];// griglia 2d of time stamps in unix epoch, will be used to calculate verified time
 
 
 public:
     // Metodi pubblici della classe ControlCenter
     ControlCenter();
     void wait();//aspetta il messaggio di "ready" dai droni
-    void addDrone(Drone drone);
-    void sendInstruction();
-    void updateMonitoringData();
+    void addDrone(); // adds a drone to the array
+    void send_instruction(); //sends the drone an istruction to start a task
+    void create_subarea(); // divides the area current(600x600) in sub_areas to be assigned to the individual drones
+    // sub areas must be of a max of 250 points - or if sqrt(250) = l, lxl -1 // 
+    void divide_tasks(); // assign the subareas to the available drones
+    void check_verified(); // checks whether a point has been verified in the last 5 min, if not logs functional requisite has been violeted - checks every point on the grid 
+    bool is_verified(); // checks single point
+    void run(); //main function, it has the loop on every drone at every istant of time t
+
+
+
+
     // ...
 };
 
--- drone/src/Drone.h
+++ drone/src/Drone.h
@@ -10,12 +10,20 @@ private:
     bool is_charging; 
     float x;
     float y;
+    const MAX_DISTANCE = 15000; // max distance in meters the drone can move with it's autonomy of 30 min
+    // the drone can never be at distance autonomy left 'd' away from its distance from the center 'c'.  if d>=c then the drone needs to go back to charge
+
 Public:
     // Costruttore e metodi pubblici della classe Drone
     Drone(int id);
     void update();
-    void chargeDrone();
-    void move();
+    void charge_drone();
+    void start_task(int x, int y); //coordinate della sottoarea
+    float get_distance_left();// returns how much distance can the drone run with the current battery level -> (battery_level * autonomia(30min)/60) * speed
+    float get_distance_control_center(); // pitagora
+    void send_verified_points(); 
+    void move_lawn_mowner(); // makes the drone move in the area in a lawn_mowner pattern
+
     // ...
 };
 
