ad0f8f732cc25a834bf8222fffcdb939b2210397
Nick <nclmda@gmail.com>
2024-01-26 22:08:48 +0100

    sistem

--- control_center/src/ControlCenter.h
+++ control_center/src/ControlCenter.h
@@ -9,15 +9,14 @@
 class ControlCenter {
 private:
     std::vector<Drone> drones;// vector of drones ids, make another drone class
-    time_t * grid[WIDTH][WIDTH];// griglia 2d of time stamps in unix epoch, will be used to calculate verified time
-
+    time_t grid[WIDTH][HEIGHT];// griglia 2d of time stamps in unix epoch, will be used to calculate verified time
 
 public:
     // Metodi pubblici della classe ControlCenter
     ControlCenter();
     void wait();//aspetta il messaggio di "ready" dai droni
-    void addDrone(); // adds a drone to the array
     void send_instruction(); //sends the drone an istruction to start a task
+
     void create_subarea(); // divides the area current(600x600) in sub_areas to be assigned to the individual drones
     // sub areas must be of a max of 250 points - or if sqrt(250) = l, lxl -1 // 
     void divide_tasks(); // assign the subareas to the available drones
